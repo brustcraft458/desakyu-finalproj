0fd791f8820a3d1a1faa4643ba24ec29fdc31dca
Bisma Candra <84393872+brustcraft458@users.noreply.github.com>
2023-12-08 23:22:50 +0700

    Fix Table Penduduk

--- table-penduduk.php
+++ table-penduduk.php
@@ -35,13 +35,13 @@ $pendudukList = loadPenduduk();
             </div>
     
             <!-- Content Row -->
-            <div class="card shadow mb-4">
+            <div class="card shadow mb-4" style="max-width: 300px">
                 <div class="card-header py-3">
                     <h6 class="m-0 font-weight-bold text-primary">Data Warga</h6>
                 </div>
 
                 <div class="table-responsive p-3">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered" id="dataTable" cellspacing="0">
                         <thead>
                             <tr>
                                 <th>Nama</th>
